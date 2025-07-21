7c9950978506d768a8b34a3433a95678cd509154
QuebecNova <selllmenya@gmail.com>
2025-07-21 10:21:59 +0700

    feat: side menu

---
+++ src/3.widgets/ui/SideMenu.tsx
@@ -0,0 +1,51 @@
+import { Box, Flex } from '@chakra-ui/react'
+import { useGameStore } from 'src/4.features/model/providers'
+import {
+    AcceptRestart,
+    Chat,
+    MatedMessage,
+    PlayedMoves,
+    Timer,
+} from 'src/4.features/ui'
+import { Colors } from 'src/6.shared/model'
+
+export function SideMenu() {
+    const players = useGameStore((state) => state.players)
+    const variant = useGameStore((state) => state.variant)
+
+    return (
+        <Box h="full">
+            <Flex
+                h="full"
+                flexDirection="column"
+                alignItems="start"
+                justifyContent="center"
+            >
+                <MatedMessage />
+                <AcceptRestart />
+                <Timer
+                    borderTopLeftRadius={0}
+                    borderBottomRightRadius={0}
+                    borderBottomLeftRadius={0}
+                    player={
+                        variant === Colors.White
+                            ? players[Colors.Black]
+                            : players[Colors.White]
+                    }
+                />
+                <PlayedMoves />
+                <Timer
+                    borderTopLeftRadius={0}
+                    borderTopRightRadius={0}
+                    borderBottomLeftRadius={0}
+                    player={
+                        variant === Colors.White
+                            ? players[Colors.White]
+                            : players[Colors.Black]
+                    }
+                />
+            </Flex>
+            <Chat />
+        </Box>
+    )
+}
