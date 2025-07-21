3235556a81550a7eb7693b44647771cab6a04f22
QuebecNova <selllmenya@gmail.com>
2025-07-21 10:20:11 +0700

    feat: played moves history

--- src/4.features/ui/game/PlayedMoves.tsx
+++ src/4.features/ui/game/PlayedMoves.tsx
@@ -1,43 +1,82 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { Card, Flex, Grid, GridItem, Text } from '@chakra-ui/react'
+import { useEffect, useRef, useState } from 'react'
 import { useGameStore } from 'src/4.features/model/providers'
+import { notation } from 'src/5.entities/lib'
+import { PlayedMove } from 'src/5.entities/model'
 import { scrollToBottom } from 'src/6.shared/lib/helpers'
-import { v4 as uuidv4 } from 'uuid'
+import { Button } from 'src/6.shared/ui'
+
+//https://en.wikipedia.org/wiki/Algebraic_notation_(chess)
 
 export default function PlayedMoves() {
     const playedMoves = useGameStore((state) => state.playedMoves)
     const lastPlayedMove = useRef<null | HTMLDivElement>(null)
-    const orderedMoves = []
+    const [currentMove, setCurrentMove] = useState(playedMoves.length - 1)
 
     useEffect(() => {
         if (!lastPlayedMove.current) return
         scrollToBottom(lastPlayedMove.current)
+        setCurrentMove(playedMoves.length - 1)
     }, [playedMoves.length])
 
-    function displayPlayedMoves(): string[] {
-        if (!playedMoves.length) return
-
-        playedMoves.forEach((move, index) => {
-            const moveText = `${index}.${move}`
-            orderedMoves.push(
-                <p ref={lastPlayedMove} key={uuidv4()}>
-                    {moveText}
-                </p>
-            )
-        })
-
-        return orderedMoves
+    function onMoveClick(move: PlayedMove, index: number) {
+        setCurrentMove(index)
     }
 
     return (
-        <div className="board__played-moves">
-            <div className="board__played-moves-header">
-                <p>Played Moves</p>
-            </div>
-            <div className="board__played-moves-content">
-                {displayPlayedMoves()}
-            </div>
-        </div>
+        <Card.Root backgroundColor="gray.800">
+            <Card.Header>
+                <Flex justifyContent="center" gap="2">
+                    <Button>prev</Button>
+                    <Button>next</Button>
+                </Flex>
+            </Card.Header>
+            <Card.Body
+                backgroundColor="gray.900"
+                p="0"
+                my="2"
+                minH="40"
+                maxH="40"
+                overflow="auto"
+            >
+                {playedMoves.length ? (
+                    <Grid templateColumns="1fr 1fr">
+                        {playedMoves.map((move, index) => (
+                            <GridItem ref={lastPlayedMove} key={index}>
+                                <Button
+                                    backgroundColor={
+                                        currentMove === index
+                                            ? 'gray.800'
+                                            : undefined
+                                    }
+                                    onClick={() => onMoveClick(move, index)}
+                                    variant="subtle"
+                                    borderRadius="0"
+                                    w="full"
+                                >
+                                    {notation.getAlgebraic(move, index)}
+                                </Button>
+                            </GridItem>
+                        ))}
+                    </Grid>
+                ) : (
+                    <Flex
+                        h="full"
+                        w="full"
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        <Text>Make a move to start</Text>
+                    </Flex>
+                )}
+            </Card.Body>
+            <Card.Footer>
+                <Button>Takeback</Button>
+                <Button>Draw</Button>
+                <Button>Resign</Button>
+            </Card.Footer>
+        </Card.Root>
     )
 }
